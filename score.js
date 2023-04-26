537b469555571f99ed8145bd6eb7999428a4d096
vipbo <liuruibo20080624@126.com>
2023-04-26 10:39:48 +0800

    Update score.js

--- score.js
+++ score.js
@@ -4,7 +4,6 @@
 		ele:document.createElement("div"),
 		mark:0,
 		init:function(){
-			this.ele.style.position="absolute";
 			this.ele.id="ball";
 			this.update(0);						//更新分数
 			document.body.appendChild(this.ele);
